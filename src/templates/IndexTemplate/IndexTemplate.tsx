0d6c15d12576e91b1ae0438d269b76ecf5d1074c
Samuel Rhea <21045301+TownLake@users.noreply.github.com>
2024-09-16 09:21:02 +0100

    Update IndexTemplate.tsx

--- src/templates/IndexTemplate/IndexTemplate.tsx
+++ src/templates/IndexTemplate/IndexTemplate.tsx
@@ -46,7 +46,7 @@ export const query = graphql`
       limit: $limit
       skip: $offset
       sort: { order: DESC, fields: [frontmatter___date] }
-      filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
+      filter: { frontmatter: { template: { eq: "post" }, category: { ne: "reading" }, draft: { ne: true } } }
     ) {
       edges {
         node {
